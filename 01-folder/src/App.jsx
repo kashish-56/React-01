c1ef7e30aef5ac44cbb4c78fc9a3d4f64b79311f
Kashish Kumari <kashish78580@gmail.com>
2026-03-23 13:42:49 +0530

    first commit in react

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+   <>
+    <div id='parent'>
+      <h1 id='child1'>Radhe Radhe</h1>
+      <h3>Hare Krishna</h3>
+    </div>
+    <div id='grandparent'></div>
+   </>
+  )
+}
+
+export default App
