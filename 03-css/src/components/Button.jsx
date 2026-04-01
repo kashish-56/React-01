36276de5af66615ed67e557eaa6d793378d79e81
Kashish Kumari <kashish78580@gmail.com>
2026-04-01 13:59:45 +0530

    03-css

---
+++ 03-css/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Button.css'
+
+const Button = () => {
+  return (
+    <div className='btn'>
+      Radhe Radhe
+    </div>
+  )
+}
+
+export default Button
---
+++ 03-css/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h3>Hare Krishna</h3>
+      <button className='btn'>Join us</button>
+    </div>
+  )
+}
+
+export default Header
