df637f11fde3e8595150d424e2edfabf45c64d0c
kashish-56 <kashish78580@gmail.com>
2026-04-12 16:40:31 +0530

    multiple-useState

--- 06-useState/src/App.jsx
+++ 06-useState/src/App.jsx
@@ -3,14 +3,36 @@ import { useState } from 'react'
 
 const App = () => {
 
-  const [num, setNum] = useState(0)
+  const [num, setNum] = useState({user: 'Radha', age: 25})
+  const [num2, setNum2] = useState([10,20,30])
+  const [num3, setNum3] = useState('Hare Krishna! ')
+
+  const btnClicked = () => {
+      const newNum = {...num};
+      newNum.age = 26;
+      newNum.user = 'Krishna';
+      setNum(newNum);
+  }
+  
+  const btnClicked2 = () => {
+      const newNum2 = [...num2];
+      newNum2[0] = 1;
+      newNum2[1] = 2;
+      newNum2[2] = 3;
+      newNum2.push(4);
+      setNum2(newNum2);
+  }
+  const btnClicked3 = () => {
+      setNum3(() => 'Hare Krishna Hari Bol !');
+  }
 
   return (
     <div>
-      <h1>{num}</h1>
-      <button onClick={()=>{setNum(num+1)}}>increase</button>
-      <button onClick={()=>{setNum(num-1)}}>decrease</button>
-      <button onClick={()=>{setNum(0)}}>reset</button>
+      <h1>{num.user}, {num.age}</h1>
+      <button onClick={btnClicked}>Click</button>
+      <h2>{num2.join(', ')}</h2>
+      <button onClick={btnClicked2}>touch me</button>
+      <button onClick={btnClicked3}>{num3}</button>
     </div>
   )
 }
